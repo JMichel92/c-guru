9edaca67cf5e1985c5ada61525faab45d43c9ba3
Jean Michel Nzi Mba <121430467+JMichel92@users.noreply.github.com>
2024-03-11 11:27:22 +0100

    Willkommen zu C-Guru

---
+++ src/main/java/com/cguru/CGuruApplication.java
@@ -0,0 +1,14 @@
+package com.cguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CGuruApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CGuruApplication.class, args);
+	}
+
+}
